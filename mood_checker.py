329bd520891a451d97a0586e4dad0bedae2df43d
poulsamiksha7 <poulsamiksha7@gmail.com>
2025-07-30 12:02:48 +0530

    Add files via upload

---
+++ mood_checker.py
@@ -0,0 +1,14 @@
+mood = input("How are you feeling today? (happy/sad/tired/sick):".lower())
+
+if mood=="happy":
+    print("Yay! Keep Smiling 😊")
+elif mood=="sad":
+    print("Sending virtual hugs🤗")
+
+elif mood=="tired":
+    print("Take some rest,you deserve it 💤")
+
+elif mood=="sick":
+    print("Drink warm water and rest. Get well soon 🤍")
+else:
+    print("Hmm... I hope your day gets better 😊")
